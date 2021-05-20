cadc2af91dda79aa424e644cc154d08435d8dc5f
Andrew Ballinger <andrewballinger@yahoo.co.uk>
2021-05-20 17:41:19 +0000

    added a vpc module

--- vpc.tf
+++ vpc.tf
@@ -3,14 +3,14 @@ module "network" {
   version = "2.5.0"
   
   project_id   = var.project
-  network_name = "hashicat-vpc"
+  network_name = "alabtest-vpc"
 
-subnets = [
-  {
-    subnet_name   = "hashicat-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+  subnets = [
+    {
+      subnet_name   = "hashicat-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
 
 }
